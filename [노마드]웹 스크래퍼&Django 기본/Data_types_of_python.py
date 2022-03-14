49ea2d72f67aed4c45f86ac49ba6fd2640c8cb07
SJUANS <lovejuan95@hanmail.net>
2022-03-14 15:31:16 +0900

    파이썬 자료형 정리

---
+++ [노마드]웹 스크래퍼&Django 기본/Data_types_of_python.py
@@ -0,0 +1,17 @@
+
+this_is_string = "문자열"
+this_is_int = 3
+this_is_float = 3.14
+this_is_boolean = True
+this_is_nonetype = None
+# 파이썬의 Boolean(True,False)와 None은 꼭 대문자로 시작해야 함!
+# None은 파이썬만 갖고 있는 자료형. JS의 null/undefined. 값이 존재하지 않음.
+# 변수 선언하고 끝에 ,를 붙이면 tuple 자료형이 됨!
+
+print(type(this_is_string))
+print(type(this_is_int))
+print(type(this_is_float))
+print(type(this_is_boolean))
+print(type(this_is_nonetype))
+# type:자료형을 반환하는 함수
+
