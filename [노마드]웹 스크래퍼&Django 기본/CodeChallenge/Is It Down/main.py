d36eec71093dcd958023dbd7c0bf035d4e7a6337
SJUANS <lovejuan95@hanmail.net>
2022-03-19 01:45:15 +0900

    response 에러 예외 처리 제외한 기능 완성

--- [노마드]웹 스크래퍼&Django 기본/CodeChallenge/Is It Down/main.py
+++ [노마드]웹 스크래퍼&Django 기본/CodeChallenge/Is It Down/main.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 def restart_Y_N():
-  YN = input("Do you want start over? y/n ") #위치 유의
+  YN = input("Do you want start over? y/n ") #input()의 위치 유의
   if YN.lower() == "y":
       os.system("clear") #콘솔창 클리어 코드(리눅스 기준)
       is_it_down()
@@ -13,10 +13,21 @@ def restart_Y_N():
     print("That's not a valid answer")
     restart_Y_N()
 
+def preprocess_url(input=""):
+  input = input.lower()
+  input = input.replace(' ','') #.strip()안먹히는데??
+  URLs = input.split(',') #,를 구분으로 쪼갠 리스트 생성
+  HTTP = "http://"
+  for URL in URLs: #주의) 여기서 URL은 URLs의 아이템을 **복사한 값**을 전달한 것 뿐이므로
+    isHTTP = HTTP in URL #boolean값을 변수에 담아야 if문에서 쓸 수 있다!
+    if isHTTP is False:
+      URLs[URLs.index(URL)] = HTTP+URL #값을 변경하고 싶다면 요렇게 해야한다
+  return URLs
+
 def is_it_down():
   print("Welcome to IsItDown.py!")
-  URL = input("Please write a URL or URLs you want to check. (Seperated by comma)\n")
-  URLs = [URL]
+  url = input("Please write a URL or URLs you want to check. (Seperated by comma)\n")
+  URLs = preprocess_url(url)
   for URL in URLs:
     URL_status = requests.get(URL).status_code
     if URL_status == 200:
