5e41d1e11fd584ae46c6265320711efee86d1b23
SJUANS <lovejuan95@hanmail.net>
2022-03-19 01:48:11 +0900

    for문+if문 부분에 주석 조금 추가

--- [노마드]웹 스크래퍼&Django 기본/CodeChallenge/Is It Down/main.py
+++ [노마드]웹 스크래퍼&Django 기본/CodeChallenge/Is It Down/main.py
@@ -18,10 +18,11 @@ def preprocess_url(input=""):
   input = input.replace(' ','') #.strip()안먹히는데??
   URLs = input.split(',') #,를 구분으로 쪼갠 리스트 생성
   HTTP = "http://"
-  for URL in URLs: #주의) 여기서 URL은 URLs의 아이템을 **복사한 값**을 전달한 것 뿐이므로
+  for URL in URLs: #주의) 여기서 URL은 URLs의 아이템을 **복사한 값**을 전달한 것 뿐!!
+    # URL=변경할 내용으로 한다 해도 실제 리스트의 항목에는 영향을 주지 못한다
     isHTTP = HTTP in URL #boolean값을 변수에 담아야 if문에서 쓸 수 있다!
     if isHTTP is False:
-      URLs[URLs.index(URL)] = HTTP+URL #값을 변경하고 싶다면 요렇게 해야한다
+      URLs[URLs.index(URL)] = HTTP+URL #실제 리스트 item의 값을 변경하고 싶다면 요렇게(인덱스 사용) 해야한다
   return URLs
 
 def is_it_down():
