21ea6bbd0334dff4566b27eae86a77547602a7b3
SJUANS <lovejuan95@hanmail.net>
2022-03-24 04:40:18 +0900

    파일분리 취소

--- [노마드]웹 스크래퍼&Django 기본/ALBA Scrapper/main.py
+++ [노마드]웹 스크래퍼&Django 기본/ALBA Scrapper/main.py
@@ -1,6 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
 
+main_result = requests.get("http://www.alba.co.kr/")
+soup = BeautifulSoup(main_result.text, "html.parser")
+main_super_brand = soup.find(id="MainSuperBrand")
+super_brands = main_super_brand.find("ul", {"class": "goodsBox"})
+super_brand_jobs = super_brands.find_all('li')
+
+def extract_SB_jobs():
+    links = []
+    for link in super_brand_jobs[:-1]:
+        links.append(link.a['href'])
+    print(links)
+
+extract_SB_jobs()
 
 
 
