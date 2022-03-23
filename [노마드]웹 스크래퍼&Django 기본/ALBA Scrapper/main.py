9f388618e746ec5304e71d34cab86e1c4c1fcd82
SJUANS <lovejuan95@hanmail.net>
2022-03-24 05:04:11 +0900

    job scrapper: 브랜드명까지 추출

--- [노마드]웹 스크래퍼&Django 기본/ALBA Scrapper/main.py
+++ [노마드]웹 스크래퍼&Django 기본/ALBA Scrapper/main.py
@@ -7,13 +7,21 @@ main_super_brand = soup.find(id="MainSuperBrand")
 super_brands = main_super_brand.find("ul", {"class": "goodsBox"})
 super_brand_jobs = super_brands.find_all('li')
 
-def extract_SB_jobs():
+def extract_SB_companies():
+    companies = []
+    for company in super_brand_jobs[:-1]:
+        company_names = (company.find("span", {"class": "company"}).string)
+        companies.append(company_names)
+    print(companies)
+
+def extract_SB_links():
     links = []
     for link in super_brand_jobs[:-1]:
         links.append(link.a['href'])
     print(links)
 
-extract_SB_jobs()
+extract_SB_companies()
+extract_SB_links()
 
 
 
