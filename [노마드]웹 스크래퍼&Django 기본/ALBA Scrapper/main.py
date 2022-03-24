50f2ee23e6df0dbc15f1a0135799a5c5328c34ad
SJUANS <lovejuan95@hanmail.net>
2022-03-24 20:26:37 +0900

    Job Scrapper 완성 (이름에 /들어갈 때 오류, 채용공고 없을 때 오류, 인코딩 설정 안했을 때 오류까지 디버깅 완)

--- [노마드]웹 스크래퍼&Django 기본/ALBA Scrapper/main.py
+++ [노마드]웹 스크래퍼&Django 기본/ALBA Scrapper/main.py
@@ -15,29 +15,40 @@ super_brand_jobs = super_brands.find_all('li')
 def extract_SB_pages():
     pages = []
     for link in super_brand_jobs[:-1]:
-        companies = (link.find("span", {"class": "company"}).string) #단일 요소에서 문자열 빼낼 때는 string
+        companies = (link.find("span", {"class": "company"}).string).replace("/","&")
         links = link.a['href']
         pages.append({"brand":companies, "job_page":links})
-        extract_SB_jobs(links)
-        # file = open(f"{companies}.csv", mode="w")
-        # 브랜드명에 /들어가면 에러 나는거 디버깅해야 함
+        extract_SB_jobs(links,companies)
+    print("-------All jobs have been scrapped successfully!-------")
 
-def extract_SB_jobs(url):
+def extract_SB_jobs(url,brand):
+    print(f"Processing {brand} jobs...")
     brand_result = requests.get(url)
     SB_soup = BeautifulSoup(brand_result.text, "html.parser")
     normal_info = SB_soup.find(id="NormalInfo").find("tbody")
     rows = normal_info.find_all("tr", {"class":""})
-
+    normal_infos = []
     for tr in rows:
-        normal_infos = []
-        places = (tr.find("td", {"class": "local first"})).get_text() #여러 요소에서 문자열 빼낼 때는 get_text()
-        titles = (tr.find("span", {"class":"company"})).get_text()
-        times = (tr.find("td", {"class":"data"})).get_text()
-        pay_text = (tr.find("td", {"class":"pay"})).get_text()
-        pays = f'"{pay_text}"'
-        dates = (tr.find("td", {"class":"regDate last"})).get_text()
+        places = tr.find("td", {"class": "local first"})
+        if places == None:
+            print(f"There is no {brand} job now.")
+            return #break 말고 return으로 해야 내용 빈 csv 안 생김
+        else:
+            places = (tr.find("td", {"class": "local first"})).get_text().replace('\xa0', ' ')
+            titles = (tr.find("span", {"class": "company"})).get_text().lstrip()
+            times = (tr.find("td", {"class": "data"})).get_text()
+            pays = (tr.find("td", {"class": "pay"})).get_text()
+            dates = (tr.find("td", {"class": "regDate last"})).get_text()
+            normal_infos.append({"place": places, "title": titles, "time": times, "pay": pays, "date": dates})
+    save_as_csv(normal_infos, brand)
 
-        print(places,titles,times,pays,dates)
+def save_as_csv(nm_infos,brands):
+    file = open(f"{brands}.csv", mode="w", encoding="UTF-8") #인코딩 설정 필요
+    writer = csv.writer(file)
+    writer.writerow(["place", "title", "time", "pay", "date"])
+    for jobs in nm_infos:
+        writer.writerow(list(jobs.values()))
+    print(f"{brands} jobs have been scrapped successfully!")
 
 extract_SB_pages()
 
