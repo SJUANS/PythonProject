b047d80409a322d0b26b1b804386109420a53eb2
SJUANS <lovejuan95@hanmail.net>
2022-05-07 02:13:49 +0900

    <오늘한잔> 프로젝트: select > select_one으로 변경

--- 칵테일 정보 크롤링/cocktail_info_detail.py
+++ 칵테일 정보 크롤링/cocktail_info_detail.py
@@ -17,18 +17,16 @@ Name = dict_result['name']
 #이미지url o
 Img = dict_result['image']
 #부재료(=가니쉬) o
-Garnish = soup.select("#sticky-anchor > div > div > div.cell.auto.divide-right-large > div > article > div > div:nth-child(2) > p")
-Garnish = Garnish[0].text
+Garnish = soup.select_one("#sticky-anchor > div > div > div.cell.auto.divide-right-large > div > article > div > div:nth-child(2) > p").text
 #맛
 #상세도수 o
-Alc_by_vol = soup.select("#sticky-anchor > div > div > div.cell.auto.divide-right-large > div > article > div > div:nth-child(14) > ul > li:nth-child(2)")
-AbV = Alc_by_vol[0].text
+AbV = soup.select_one("#sticky-anchor > div > div > div.cell.auto.divide-right-large > div > article > div > div:nth-child(14) > ul > li:nth-child(2)").text
 Floats_in_AbV = re.findall("\d+.\d+",AbV) #정규표현식으로 AbV라는 문자열 안에 있는 숫자 두개 추출
-AbV = Floats_in_AbV[0]
+Alc_by_vol = Floats_in_AbV[0]
 #레시피 재료 o
 Ingredients = dict_result['recipeIngredient']
 #레시피 o
 Recipe = dict_result['recipeInstructions'][0]['text']
 
-# print(Name,Img,Ingredients,Recipe)
+print(Name,Img,Garnish,Alc_by_vol,Ingredients,Recipe)
 
