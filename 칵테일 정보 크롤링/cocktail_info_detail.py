c91f2e99c6335dfd56ed331adc0bd841a116b567
SJUANS <lovejuan95@hanmail.net>
2022-05-07 03:41:35 +0900

    <오늘한잔> 프로젝트: 칵테일 정보 딕셔너리형으로 출력까지 완료(JSON 변환 전)

--- 칵테일 정보 크롤링/cocktail_info_detail.py
+++ 칵테일 정보 크롤링/cocktail_info_detail.py
@@ -3,47 +3,63 @@ import re #파이썬 정규표현식 라이브러리
 from bs4 import BeautifulSoup
 import requests
 
-cocktail_detail_Triple_Daiquiri = "https://www.diffordsguide.com/cocktails/recipe/4809/triple-daiquiri"
-page_Triple_Daiquiri = requests.get(cocktail_detail_Triple_Daiquiri)
-soup = BeautifulSoup(page_Triple_Daiquiri.text, "html.parser")
+def extract_cocktail_collection():
+    cocktail_detail_Triple_Daiquiri = "https://www.diffordsguide.com/cocktails/recipe/4809/triple-daiquiri"
+    page_Triple_Daiquiri = requests.get(cocktail_detail_Triple_Daiquiri)
+    soup = BeautifulSoup(page_Triple_Daiquiri.text, "html.parser")
 
-#크롤링으로 수집 가능 정보↓
-#<script>태그 안 JSON에 저장된 정보
-cocktail_info = soup.find("script", {"type": "application/ld+json"})
-string_cocktail_info = cocktail_info.text #HTML 요소 내 JSON으로 작성된 부분 문자열화
-dict_result = json.loads(string_cocktail_info) #문자열화한 결과를 다시 JSON으로 변환
+    # 크롤링으로 수집 가능 정보↓
+    # <script>태그 안 JSON에 저장된 정보
+    cocktail_page = soup.find("script", {"type": "application/ld+json"})
+    string_cocktail_page = cocktail_page.text  # HTML 요소 내 JSON으로 작성된 부분 문자열화
+    dict_result = json.loads(string_cocktail_page)  # 문자열화한 결과를 다시 JSON으로 변환
 
-#칵테일 이름 o
-Name = dict_result['name']
-#이미지url o
-Img = dict_result['image']
-#부재료(=가니쉬) o
-Garnish = soup.select_one("#sticky-anchor > div > div > div.cell.auto.divide-right-large > div > article > div > div:nth-child(2) > p").text
-#맛
-Flavor = soup.select_one("#sticky-anchor > div > div > div.cell.auto.divide-right-large > div > article > div > div:nth-child(10) > div > div:nth-child(2) > div > div > div:nth-child(3) > img")
-Flavor_level = Flavor["alt"]
-#상세도수 o
-AbV = soup.select_one("#sticky-anchor > div > div > div.cell.auto.divide-right-large > div > article > div > div:nth-child(14) > ul > li:nth-child(2)").text
-Floats_in_AbV = re.findall("\d+.\d+",AbV) #정규표현식으로 AbV라는 문자열 안에 있는 숫자 추출
-Alc_by_vol = float(Floats_in_AbV[0])
-#도수(범주형) o
-AbV_level = "Undefined"
-if Alc_by_vol <= 10:
-    AbV_level = "1"
-elif Alc_by_vol <= 20:
-    AbV_level = "2"
-elif Alc_by_vol <= 30:
-    AbV_level = "3"
-elif Alc_by_vol <= 40:
-    AbV_level = "4"
-elif Alc_by_vol <= 50:
-    AbV_level = "5"
-elif Alc_by_vol >= 51:
-    AbV_level = "6"
-#레시피 재료 o
-Ingredients = dict_result['recipeIngredient']
-#레시피 o
-Recipe = dict_result['recipeInstructions'][0]['text']
+    # 칵테일 이름 o
+    Name = dict_result['name']
+    # 이미지url o
+    Img = dict_result['image']
+    # 부재료(=가니쉬) o
+    Garnish = soup.select_one(
+        "#sticky-anchor > div > div > div.cell.auto.divide-right-large > div > article > div > div:nth-child(2) > p").text
+    # 맛
+    Flavor = soup.select_one(
+        "#sticky-anchor > div > div > div.cell.auto.divide-right-large > div > article > div > div:nth-child(10) > div > div:nth-child(2) > div > div > div:nth-child(3) > img")
+    Flavor_level = Flavor["alt"]
+    # 상세도수 o
+    AbV = soup.select_one(
+        "#sticky-anchor > div > div > div.cell.auto.divide-right-large > div > article > div > div:nth-child(14) > ul > li:nth-child(2)").text
+    Floats_in_AbV = re.findall("\d+.\d+", AbV)  # 정규표현식으로 AbV라는 문자열 안에 있는 숫자 추출
+    Alc_by_vol = float(Floats_in_AbV[0])  # 추출한 숫자의 자료형: 문자열>실수 로 변환
+    # 도수(범주형) o
+    AbV_level = "Undefined"
+    if Alc_by_vol <= 10:
+        AbV_level = "1"
+    elif Alc_by_vol <= 20:
+        AbV_level = "2"
+    elif Alc_by_vol <= 30:
+        AbV_level = "3"
+    elif Alc_by_vol <= 40:
+        AbV_level = "4"
+    elif Alc_by_vol <= 50:
+        AbV_level = "5"
+    elif Alc_by_vol >= 51:
+        AbV_level = "6"
+    # 레시피 재료 o
+    Ingredients = dict_result['recipeIngredient']
+    # 레시피 o
+    Recipe = dict_result['recipeInstructions'][0]['text']
 
-print(Name,Img,Garnish,Alc_by_vol,AbV_level,Flavor_level,Ingredients,Recipe)
+    cocktail_data = {
+        "이름" : Name,
+        "썸네일" : Img,
+        "부재료" : Garnish,
+        "맛" : Flavor_level,
+        "도수" : AbV_level,
+        "상세 도수" : Alc_by_vol,
+        "재료" : Ingredients,
+        "레시피" : Recipe
+    }
+    print(cocktail_data)
+extract_cocktail_collection()
+
 
