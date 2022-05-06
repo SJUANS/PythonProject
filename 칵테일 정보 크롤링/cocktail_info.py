b3956efa0c7ea08ff2f4e54110a50001a98da8f6
SJUANS <lovejuan95@hanmail.net>
2022-05-06 18:40:01 +0900

    <오늘한잔> 프로젝트: 상세페이지-이름, 이미지, 레시피(재료+방법) 추출 완료

--- 칵테일 정보 크롤링/cocktail_info.py
+++ 칵테일 정보 크롤링/cocktail_info.py
@@ -9,17 +9,20 @@ soup = BeautifulSoup(page_Triple_Daiquiri.text, "html.parser")
 
 #크롤링으로 수집 가능 정보↓
 cocktail_info = soup.find("script", {"type": "application/ld+json"})
-string_cocktail_info = cocktail_info.text #HTML 요소 내 JSON을 문자열화
+string_cocktail_info = cocktail_info.text #HTML 요소 내 JSON으로 작성된 부분 문자열화
 dict_result = json.loads(string_cocktail_info) #문자열화한 결과를 다시 JSON으로 변환
 
-print(dict_result)
-
 #칵테일 이름 o
-
-#Name =
-#이미지url
+Name = dict_result['name']
+#이미지url o
+Img = dict_result['image']
 #부재료(=가니쉬)
 #맛
 #상세도수
-#레시피 재료
+#레시피 재료 o
+Ingredients = dict_result['recipeIngredient']
 #레시피 o
+Recipe = dict_result['recipeInstructions'][0]['text']
+
+print(Name,Img,Ingredients,Recipe)
+
