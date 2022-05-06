82fbf82c21c9c0ba953f6e58e7706ead91db9d63
SJUANS <lovejuan95@hanmail.net>
2022-05-06 18:24:13 +0900

    <오늘한잔> 프로젝트: HTML script 태그 내 JSON 크롤링

--- 칵테일 정보 크롤링/cocktail_info.py
+++ 칵테일 정보 크롤링/cocktail_info.py
@@ -1,3 +1,5 @@
+import json
+
 from bs4 import BeautifulSoup
 import requests
 
@@ -5,12 +7,19 @@ cocktail_detail_Triple_Daiquiri = "https://www.diffordsguide.com/cocktails/recip
 page_Triple_Daiquiri = requests.get(cocktail_detail_Triple_Daiquiri)
 soup = BeautifulSoup(page_Triple_Daiquiri.text, "html.parser")
 
-print(soup)
-
 #크롤링으로 수집 가능 정보↓
-#칵테일 이름
+cocktail_info = soup.find("script", {"type": "application/ld+json"})
+string_cocktail_info = cocktail_info.text #HTML 요소 내 JSON을 문자열화
+dict_result = json.loads(string_cocktail_info) #문자열화한 결과를 다시 JSON으로 변환
+
+print(dict_result)
+
+#칵테일 이름 o
+
+#Name =
 #이미지url
 #부재료(=가니쉬)
 #맛
 #상세도수
-#레시피
+#레시피 재료
+#레시피 o
